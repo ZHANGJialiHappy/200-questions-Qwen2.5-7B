a3dc75cc651f2d872c4f719067b4be594c1391e8
Jiali Zhang <zhangjiali86@gmail.com>
2024-10-21 08:39:38 +0200

    upload

---
+++ qa.py
@@ -0,0 +1,36 @@
+from transformers import AutoModelForCausalLM
+from transformers import AutoTokenizer
+import torch
+
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+lm = 'Qwen/Qwen2.5-7B'
+token = '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'
+lang_model = AutoModelForCausalLM.from_pretrained(lm, token=token)
+lang_model.to(DEVICE)
+tokenizer = AutoTokenizer.from_pretrained(lm, token=token, use_fast=False)
+
+questions = open('questions.csv').readlines()
+
+with open('Qwen2.5_7B_answer.txt', 'w') as answer_file:
+    prefixes = ['']
+    postfixes = ['']
+
+    for prefix, postfix in zip(prefixes, postfixes):
+        for question in questions:
+            question = prefix + ' ' + question.strip() + ' ' + postfix
+            tokked = tokenizer(question.strip(), return_tensors='pt', truncation=True, padding=True)['input_ids']
+            tokked = tokked.to(DEVICE)
+            generated_ids = lang_model.generate(tokked, max_new_tokens=200)
+            tokens = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+
+            # 打印问题和答案
+            print(question)
+            print(' '.join(tokens))
+            print()
+
+            # 将答案写入文件
+            # answer_file.write('Question: ' + question + '\n')
+            answer_file.write('Answer: ' + ' '.join(tokens) + '\n\n')
+            
+
